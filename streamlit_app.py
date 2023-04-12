53917f0f88a63a8ba79d6154489e1b6d70bb5999
Yoza Saputra Utama <113530814+YozaSaputra@users.noreply.github.com>
2023-04-12 22:59:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,10 +16,10 @@ st.write("""
 ## [Dataset](https://www.kaggle.com/datasets/mehmettahiraslan/customer-shopping-dataset)
 
 ## Latar Belakang Masalah
-Pada tahun 2020 tepatnya pada bulan Maret, terjadi suatu pandemi yang melanda seluruh dunia, yaitu pandemi Covid 19. Pandemi ini menyebabkan masalah yang serius karena banyak negara yang mengalami kerugian akibat adanya pandemi ini, salah satunya adalah kerugian ekonomi. Salah satu object yang terdampak dari pandemi adalah Shopping Mall. Oleh karena itu, kami ingin menganalisa tren dan kebiasaan belanja di negara Istanbul selama masa pandemi dengan menggunakan data dari 10 Mall pada periode tahun 2021-2022.
 """)
 
-st.markdown('<div style="text-align: center;">Hello World!</div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align: justify;">Pada tahun 2020 tepatnya pada bulan Maret, terjadi suatu pandemi yang melanda seluruh dunia, yaitu pandemi Covid 19. Pandemi ini menyebabkan masalah yang serius karena banyak negara yang mengalami kerugian akibat adanya pandemi ini, salah satunya adalah kerugian ekonomi. Salah satu object yang terdampak dari pandemi adalah Shopping Mall. Oleh karena itu, kami ingin menganalisa tren dan kebiasaan belanja di negara Istanbul selama masa pandemi dengan menggunakan data dari 10 Mall pada periode tahun 2021-2022.
+</div>', unsafe_allow_html=True)
 
 # Read the CSV file into a DataFrame
 data = pd.read_csv("customer_shopping_data.csv")
