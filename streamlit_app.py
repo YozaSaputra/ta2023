c44b2122194b535fd0d533e0d1126d4554681b65
Yoza Saputra Utama <113530814+YozaSaputra@users.noreply.github.com>
2023-04-12 23:02:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,9 @@ import pandas as pd
 import streamlit as st
 
 
-st.write("""
-# Analisis Tren Transaksi Pembelanjaan di Istanbul Periode 2021-2023
+st.markdown('<div style="text-align: center;">Analisis Tren Transaksi Pembelanjaan di Istanbul Periode 2021-2023 </div>', unsafe_allow_html=True)
 
+st.write("""
 ### Kelompok Lima:
 - ### Sonia Epifany Sandah
 - ### Sugih Gumilar
