18490f842d9c1378120c0af4817037606d29c33b
Yoza Saputra Utama <113530814+YozaSaputra@users.noreply.github.com>
2023-04-12 22:58:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,8 @@ st.write("""
 Pada tahun 2020 tepatnya pada bulan Maret, terjadi suatu pandemi yang melanda seluruh dunia, yaitu pandemi Covid 19. Pandemi ini menyebabkan masalah yang serius karena banyak negara yang mengalami kerugian akibat adanya pandemi ini, salah satunya adalah kerugian ekonomi. Salah satu object yang terdampak dari pandemi adalah Shopping Mall. Oleh karena itu, kami ingin menganalisa tren dan kebiasaan belanja di negara Istanbul selama masa pandemi dengan menggunakan data dari 10 Mall pada periode tahun 2021-2022.
 """)
 
+st.markdown('<div style="text-align: center;">Hello World!</div>', unsafe_allow_html=True)
+
 # Read the CSV file into a DataFrame
 data = pd.read_csv("customer_shopping_data.csv")
 # Display the DataFrame in the app
