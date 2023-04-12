50b517ec67d730521c2679401a3c93154829d1aa
Yoza Saputra Utama <113530814+YozaSaputra@users.noreply.github.com>
2023-04-12 22:56:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,9 @@ st.write("""
 - ### Yoza Saputra Utama
 
 ## [Dataset](https://www.kaggle.com/datasets/mehmettahiraslan/customer-shopping-dataset)
+
+## Latar Belakang Masalah
+Pada tahun 2020 tepatnya pada bulan Maret, terjadi suatu pandemi yang melanda seluruh dunia, yaitu pandemi Covid 19. Pandemi ini menyebabkan masalah yang serius karena banyak negara yang mengalami kerugian akibat adanya pandemi ini, salah satunya adalah kerugian ekonomi. Salah satu object yang terdampak dari pandemi adalah Shopping Mall. Oleh karena itu, kami ingin menganalisa tren dan kebiasaan belanja di negara Istanbul selama masa pandemi dengan menggunakan data dari 10 Mall pada periode tahun 2021-2022.
 """)
 
 # Read the CSV file into a DataFrame
